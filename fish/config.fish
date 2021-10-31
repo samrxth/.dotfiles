9a794dc70b655a86f1e9e6c25bfdc7a2f618657f
samrath2007 <samrath1324@gmail.com>
2021-10-31 09:23:35 +0530

    fix kitty sync script

--- fish/config.fish
+++ fish/config.fish
@@ -57,7 +57,7 @@ function updateDots
   cp ~/.config/iterm2/com.googlecode.iterm2.plist ~/.dotfiles/iterm2/
   cp ~/.config/fish/config.fish ~/.dotfiles/fish
   cp ~/.config/alacritty/alacritty.yml ~/.dotfiles/alacritty/alacritty.yml
-  rsync -rv ~/.config/kitty ~/.dotfiles/kitty
+  rsync -rv ~/.config/kitty ~/.dotfiles
   cp ~/.tmux.conf ~/.dotfiles/tmux/tmux.conf
   rsync -rv ~/.config/zathura/ ~/.dotfiles/zathura
 end
