9f24a467e3af99ed1a1e57f0df89546023c562bb
samrath2007 <samrath1324@gmail.com>
2021-12-09 16:56:29 +0530

    move back to fish and add starship

--- fish/config.fish
+++ fish/config.fish
@@ -103,3 +103,5 @@ source ~/.config/fish/colors.fish
 # Setting PATH for Python 3.10
 # The original version is saved in /Users/sam/.config/fish/config.fish.pysave
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+starship init fish | source
